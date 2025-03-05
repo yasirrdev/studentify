3d5ecee6c8a98dc43687cae92ffe9a97d4a5a74f
David Hormigo <dhorram948@g.educaand.es>
2025-03-05 10:56:16 +0100

    Fix rename

--- deploy-scripts/beforeinstall.sh
+++ deploy-scripts/beforeinstall.sh
@@ -2,4 +2,4 @@
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 chmod +x gradlew
 ./gradlew build
-mv studentify-1.0.0.war studentify.war
+mv build/libs/studentify-1.0.0.war studentify.war
