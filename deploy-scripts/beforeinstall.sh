a8c92873778cae2f50faad67ff930a54850510b8
Yasir <y.asirbelmaalem30@gmail.com>
2025-03-07 10:49:49 +0100

    Update beforeinstall.sh

--- deploy-scripts/beforeinstall.sh
+++ deploy-scripts/beforeinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y openjdk-21-jdk
+# sudo apt-get update -y
+# sudo apt-get install -y openjdk-21-jdk
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 chmod +x gradlew
